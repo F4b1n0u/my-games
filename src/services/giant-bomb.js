66db46bc35f32b2e6411be2b3935106e9cc3dc03
Fabien BEHIER <fabien.behier@icloud.com>
2017-08-21 11:49:42 +0100

    add query param generation

--- src/services/giant-bomb.js
+++ src/services/giant-bomb.js
@@ -1,10 +1,26 @@
-import 'rxjs';
-import { ajax } from 'rxjs/observable/dom/ajax';
-
+import 'rxjs'
+import { ajax } from 'rxjs/observable/dom/ajax'
+import qs from "query-string"
+import _ from 'lodash'
 import {
   API_KEY,
 } from 'react-native-dotenv'
 
+const defaultQueryParams = {
+  api_key: API_KEY,
+  format: 'json',
+}
+
 export function fetchSuggestions(searchText) {
-  return ajax.getJSON(`http://www.giantbomb.com/api/search/?api_key=${API_KEY}&format=json&query="${searchText}"&resources[0]=game&field_list=name,image`)
+  const queryParams = _.merge(
+    defaultQueryParams,
+    {
+      query: searchText,
+      resources: "game",
+      field_list: "name,image,id",
+      limit: 6,
+    }
+  )
+
+  return ajax.getJSON(`http://www.giantbomb.com/api/search/?${qs.stringify(queryParams)}`)
 }
