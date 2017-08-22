add05eeac3c43a26cd1a420ed33c96c0517b1d9e
Fabien BEHIER <fabien.behier@icloud.com>
2017-08-22 14:18:12 +0100

    add complete game marking

--- app/services/giant-bomb.js
+++ app/services/giant-bomb.js
@@ -11,6 +11,16 @@ const defaultQueryParams = {
   format: 'json',
 }
 
+const markGamesAsComplete = response => {
+  response.results = response.results.map(game => _.merge(game, {isComplet: true}))
+  return response
+}
+
+const markGamesAsIncomplete = response => {
+  response.results = response.results.map(game => _.merge(game, {isComplet: false}))
+  return response
+}
+
 export const fetchFranchiseFranchises = (searchText) => {
   const queryParams = _.merge(
     defaultQueryParams,
@@ -31,13 +41,29 @@ export const fetchGames = (searchText) => {
   const queryParams = _.merge(
     defaultQueryParams,
     {
-      query: `%${searchText}%`,
-      resources: [
-        'game'
-      ].join(','),
-      field_list: "id,name,image,deck,api_detail_url,platforms",
+      filter: `name:%${searchText.replace(' ', '%')}%`,
+      sort: 'number_of_user_reviews:desc',
+      field_list: "id,name,image,deck,platforms",
     }
   )
 
-  return ajax.getJSON(`https://www.giantbomb.com/api/search/?${qs.stringify(queryParams)}`)
+  return ajax
+    .getJSON(`https://www.giantbomb.com/api/games/?${qs.stringify(queryParams)}`)
+    .map(markGamesAsComplete)
+}
+
+export const fetchGame = ({id}) => {
+  const gamePrefix = 3030 // don;t ask me why see that with giantBomb xD, looks like the prefix depend of the endpoint
+  const queryParams = _.merge(
+    defaultQueryParams,
+    {
+      filter: `name:%${searchText.replace(' ', '%')}%`,
+      sort: 'number_of_user_reviews:desc',
+      field_list: 'id,name,image,deck,platforms'
+    }
+  )
+
+  return ajax
+    .getJSON(`https://www.giantbomb.com/api/game/${gamePrefix}-${id}?${qs.stringify(queryParams)}`)
+    .map(markGamesAsComplete)
 }
