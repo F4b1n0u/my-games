e60f3d5a923f0f3c70e89cf53b0d2b672e6fb50a
Fabien BEHIER <fabien.behier@icloud.com>
2017-09-03 19:44:39 +0100

    add sentry

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 
 import Expo from 'expo'
 import React from 'react'
+import Sentry from 'sentry-expo'
 import { Provider } from 'react-redux'
 
 import storeFactory from '@store'
@@ -15,5 +16,11 @@ const Index = () => (
     <App />
   </Provider>
 )
+
+Sentry.enableInExpoDevelopment = true
+
+Sentry
+  .config('https://577e0a8ab9124013aaa3c43b6209d0b5:9af1ee2a9e234a7a8f4686bfeceffe04@sentry.io/212109')
+  .install()
 
 Expo.registerRootComponent(Index)
