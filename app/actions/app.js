f0502a56196c93df85ca3f19b67ba4a3c08a839a
Fabien BEHIER <fabien.behier@icloud.com>
2017-09-06 17:07:49 +0100

    add generic error display

--- app/actions/app.js
+++ app/actions/app.js
@@ -2,6 +2,7 @@ export const START_LOAD = 'my-games/app/START_LOAD'
 export const END_LOAD_SUCCESS = 'my-games/app/END_LOAD_SUCCESS'
 export const END_LOAD_FAILURE = 'my-games/app/END_LOAD_FAILURE'
 export const TOGGLE_ABOUT_DISPLAY = 'my-games/app/TOGGLE_ABOUT_DISPLAY'
+export const DISPLAY_GENERIC_ERROR = 'my-games/app/DISPLAY_GENERIC_ERROR'
 
 export const startLoad = () => ({
   type: START_LOAD,
@@ -14,5 +15,9 @@ export const endLoadFailure = () => ({
 })
 export const toggleAboutDisplay = () => ({
   type: TOGGLE_ABOUT_DISPLAY,
+})
+export const displayGenericError = error => ({
+  type: DISPLAY_GENERIC_ERROR,
+  error,
 })
 
--- app/epics/index.js
+++ app/epics/index.js
@@ -1,11 +1,13 @@
 import { combineEpics } from 'redux-observable'
 
+import app from '@epics/app'
 import searchEngineEpic from '@epics/search-engine'
 import gameCatalogueEpic from '@epics/game-catalogue'
 import gameExplorerEpic from '@epics/game-explorer'
 import ownedGameCatalogue from '@epics/owned-game-catalogue'
 
 export default combineEpics(
+  app,
   searchEngineEpic,
   gameCatalogueEpic,
   gameExplorerEpic,
