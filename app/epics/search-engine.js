65fea463ce7fe0912d53172a7b850b3858328079
Fabien BEHIER <fabien.behier@icloud.com>
2017-09-11 09:23:19 +0100

    disable async optimisisation for now to avoid bugs

--- app/epics/search-engine.js
+++ app/epics/search-engine.js
@@ -67,7 +67,7 @@ const requestFranchisesEpic = (action$, store) => action$
         // Req G <- similar to STOP_SEARCHING here due to requestGamesToStopEpic
         // Res G
         // Res F
-        .takeUntil(action$.ofType(STOP_SEARCHING))
+        // .takeUntil(action$.ofType(STOP_SEARCHING))
         .catch(error => Observable.of(receiveFranchisesFailure(error)))
     } else {
       observable = Observable.of(receiveFranchises([]))
@@ -91,7 +91,7 @@ const requestFranchiseCompletionEpic = action$ => action$
       response.results.games,
       extractPagination(response)
     ))
-    .takeUntil(action$.ofType(SUBMIT_SEARCH))
+    // .takeUntil(action$.ofType(SUBMIT_SEARCH))
     .catch(error => Observable.of(receiveFranchiseCompletionFailure(error)))
   )
 
